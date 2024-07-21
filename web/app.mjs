10f89990843e098c4a8fc999659f1f5667fa367d
yu1c1yu1c1 <hirokikayama98@gmail.com>
2024-07-21 16:51:07 +0900

    add sign up page

--- web/app.mjs
+++ web/app.mjs
@@ -9,15 +9,32 @@ const PORT = 2222;
 
 const methodNotAllowed = (req, res, next) => res.status(405).send();
 
-
-// DB
-main().catch(err => console.log(err));
+// connect to the DB
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/users');
+  await mongoose.connect("mongodb://mongodb:27017/users");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+// create the schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "enter task name"],
+    trim: true,
+    maxlength: [20, "max length: 20 letters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// create the model
+
+const User = mongoose.model("User", userSchema);
 
 // トップページ
 app
@@ -27,18 +44,38 @@ app
   })
   .all(methodNotAllowed);
 
+// 新規登録
+app
+  .route("/signup")
+  .get((req, res) => {
+    res.render("signup.ejs");
+  })
+  .post((req, res) => {
+    //登録処理
+  });
+
 // ログイン画面
 app
   .route("/login")
   .get((req, res) => {
     res.render("login.ejs");
   })
+  .post((req, res) => {
+    if (true) {
+      // 認証成功
+      res.render("index.ejs", { name: "User" });
+    } else {
+      // 認証失敗
+      res.render("index.ejs").status(401);
+    }
+    res.send("Welcome to about page");
+  })
   .all(methodNotAllowed);
 
 app.post("/api/v1/login", (req, res) => {
   if (true) {
     // 認証成功
-    res.render("index.ejs", { name:"User"});
+    res.render("index.ejs", { name: "User" });
   } else {
     // 認証失敗
     res.render("index.ejs").status(401);
